d25e6f9b2f063a21688497b375241fdddfd1641f
Thanush-07 <thanushkumar722@gmail.com>
2025-08-14 13:42:54 -0400

    app file updated

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/instruction')
+def instruction():
+    return render_template('instruction.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
