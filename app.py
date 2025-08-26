64ef05bfb64a9a26157d6c5c9dca820b070c253e
Thanush-07 <178015554+Thanush-07@users.noreply.github.com>
2025-08-26 19:11:54 +0530

    flask file updated

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import threading
+import sniffer
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/instruction')
-def instruction():
-    return render_template('instruction.html')
+# Start sniffing in background thread (set iface here)
+threading.Thread(target=sniffer.start_sniffing, args=("Wi-Fi",), daemon=True).start()
 
-if __name__ == '__main__':
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/packets")
+def get_packets():
+    return jsonify(sniffer.packets[-20:])  # last 20 packets
+
+if __name__ == "__main__":
     app.run(debug=True)
